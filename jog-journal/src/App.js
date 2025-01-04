94ea98563183fa25567271d0a2d8a8949ff55d96
Hayden <haysay424@gmail.com>
2025-01-04 21:47:44 +1300

    initial commit. Created project and removed default content. Added header and slogan.

---
+++ jog-journal/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jog Journal</h1>
+      <p>Your companion for tracking every step towards your yearly goal.</p>
+    </div>
+  );
+}
+
+export default App;
