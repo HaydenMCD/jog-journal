29b88a5040ba0b054f4d4700199ebb34fa64a3d2
Hayden <haysay424@gmail.com>
2025-01-10 18:53:33 +1300

    Added submit button. Wrapped the input in a form. Added basic css. Added title for kilometer input section

--- src/components/KilometerInput.js
+++ src/components/KilometerInput.js
@@ -7,20 +7,30 @@ const KilometerInput = () => {
   const increase = () => setCount(count + 1);
   const decrease = () => setCount(count - 1);
 
+  const handleSubmit = () => {
+    window.location.reload(false);
+  };
+
   return (
     <div className="kilometerContainer">
-      <button className="decreaseButton" onClick={decrease}>
-        -
-      </button>
-      <input
-        type="text"
-        value={count}
-        readOnly
-        className="counterInput"
-        aria-label="NumberInput"
-      />
-      <button className="increaseButton" onClick={increase}>
-        +
+        <p className="inputTitle">Enter your kilometers ran today</p>
+      <form className="counterForm" onSubmit={handleSubmit}>
+        <button className="decreaseButton" type="button" onClick={decrease}>
+          -
+        </button>
+        <input
+          type="text"
+          value={count}
+          readOnly
+          className="counterInput"
+          aria-label="numberInput"
+        />
+        <button className="increaseButton" type="button" onClick={increase}>
+          +
+        </button>
+      </form>
+      <button className="submitButton" onClick={handleSubmit}>
+        Submit
       </button>
     </div>
   );
