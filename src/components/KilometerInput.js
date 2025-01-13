ac3704c53e7477d101b57d5695a53c870d5cb5fa
Hayden <haysay424@gmail.com>
2025-01-13 22:12:31 +1300

    Added addDoc function when form submitted. Created a collection with called {year} runs (if not already there). Then adds the kilometers entered and timestamp.

--- src/components/KilometerInput.js
+++ src/components/KilometerInput.js
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
 import "../css/KilometerInput.css";
+import { firestore } from "../firebase";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
 
 const KilometerInput = () => {
-  const [count, setCount] = useState(0);
+  const [kmCount, setKmCount] = useState(0);
 
-  const increase = () => setCount(count + 1);
-  const decrease = () => setCount(count - 1);
+  const increase = () => setKmCount(kmCount + 1);
+  const decrease = () => setKmCount(kmCount - 1);
 
-  const handleSubmit = () => {
-    window.location.reload(false);
+  const year = new Date().getFullYear();
+
+  const collectionRef = collection(firestore, `${year} Runs`);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      addDoc(collectionRef, { Kilometers: kmCount, date: Timestamp.now() });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <div className="kilometerContainer">
-        <p className="inputTitle">Enter your kilometers ran today</p>
+      <p className="inputTitle">Enter your kilometers ran today</p>
       <form className="counterForm" onSubmit={handleSubmit}>
         <button className="decreaseButton" type="button" onClick={decrease}>
           -
         </button>
         <input
           type="text"
-          value={count}
+          value={kmCount}
           readOnly
           className="counterInput"
           aria-label="numberInput"
