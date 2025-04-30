3f24688c61bbe5302368361992d15e2f6b29ab92
Anthony Legg <anthony.legg@outlook.com>
2025-05-01 11:26:00 +1200

    #9 update constant file
    add commented sections

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,26 @@
-// Constants for distance conversions
-export const CONVERSION_TO_MILES = 0.621371; // Conversion factor from kilometers to miles
-export const CONVERSION_TO_KILOMETERS = 1.60934; // Conversion factor from miles to kilometers
+/* -------------------------------------------------------------------------- */
+/*                     Constants for distance conversions                     */
+/* -------------------------------------------------------------------------- */
 
-// Constants for time conversions
-export const MILISECONDS_IN_DAY = 86400000; // Number of milliseconds in a day
-export const MILISECONDS_IN_HOUR = 3600000; // Number of milliseconds in an hour
+// Conversion factor from kilometers to miles
+export const CONVERSION_TO_MILES = 0.621371;
+// Conversion factor from miles to kilometers
+export const CONVERSION_TO_KILOMETERS = 1.60934;
+
+/* -------------------------------------------------------------------------- */
+/*                       Constants for time conversions                       */
+/* -------------------------------------------------------------------------- */
+
+// Number of milliseconds in a day
+export const MILISECONDS_IN_DAY = 86400000;
+// Number of milliseconds in an hour
+export const MILISECONDS_IN_HOUR = 3600000;
+
+/* -------------------------------------------------------------------------- */
+/*                        Constants for reducer actions                       */
+/* -------------------------------------------------------------------------- */
+
+// Action type for adding distance as miles
+export const SET_MILES = ["SET_MILES", CONVERSION_TO_MILES];
+// Action type for adding distance as kilometers
+export const SET_KILOMETERS = ["SET_KILOMETERS", CONVERSION_TO_KILOMETERS];
