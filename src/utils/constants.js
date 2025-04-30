33c7d3a050c0690fa14efc560c4518bc5676a570
Anthony Legg <anthony.legg@outlook.com>
2025-04-30 17:53:28 +1200

    create `src\utils\constants.js`
    export site wide variables

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+// Constants for distance conversions
+export const MILES = 0.621371; // Conversion factor from kilometers to miles
+export const KILOMETERS = 1.60934; // Conversion factor from miles to kilometers
+
+// Constants for time conversions
+export const MILISECONDS_IN_DAY = 86400000; // Number of milliseconds in a day
+export const MILISECONDS_IN_HOUR = 3600000; // Number of milliseconds in an hour
