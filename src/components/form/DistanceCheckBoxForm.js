ec8140ef6a9ba34bba4db32e273b021c715ba275
Anthony Legg <anthony.legg@outlook.com>
2025-05-13 11:26:50 +1200

    #9 update distance checkbox form component
    add additional props for state and functions passed from the outer component

--- src/components/form/DistanceCheckBoxForm.js
+++ src/components/form/DistanceCheckBoxForm.js
@@ -1,24 +1,29 @@
-import { memo } from 'react';
-
 /**
  *
  * @param {boolean} kilometersOn - true kilometers is selected, false miles is selected
+ * @param {function} setKilometersOn - function to set kilometersOn
+ * @param {('Kilometers' | 'Miles')} distanceType - type of distance | Kilometers or Miles
+ * @returns {JSX.Element} - DistanceCheckBoxForm component
  */
 
-const DistanceCheckBox = memo(function DistanceCheckBox(kilometersOn) {
+function DistanceCheckBox(kilometersOn, setKilometersOn, distanceType) {
   return (
     <form>
-      <label htmlFor={kilometersOn ? 'kilometers' : 'miles'}>
-        {kilometersOn ? 'Kilometers' : 'Miles'}
+      <label htmlFor={distanceType}>
+        {distanceType}
       </label>
       <input
         type={'checkbox'}
-        defaultChecked={kilometersOn}
+        defaultChecked={ kilometersOn }
+        name={ 'distanceType' }
+        onChange={(e) => {
+          setKilometersOn(e.target.checked);
+        }}
         id={kilometersOn ? 'kilometers' : 'miles'}
         value={kilometersOn}
       />
     </form>
   );
-});
+}
 
 export default DistanceCheckBox;
