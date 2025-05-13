0dacf13e41bf41fd80fcebf192661af888f7fa8c
Anthony Legg <anthony.legg@outlook.com>
2025-05-13 13:29:13 +1200

    #9 add state import into `useLocalStorage` hook

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,4 +1,6 @@
-export const useLocalStorage = (key, initialValue) => { 
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = window.localStorage.getItem(key);
@@ -21,4 +23,4 @@ export const useLocalStorage = (key, initialValue) => {
   };
 
   return [storedValue, setValue];
-}
+};
