2b8e5679b47396f206068f7f47df714b7c529ca0
Anthony Legg <anthony.legg@outlook.com>
2025-05-18 18:41:53 +1200

    #9 update component to use constant.js
    format file with prettier

--- src/components/KilometerInput.jsx
+++ src/components/KilometerInput.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
-import "../css/KilometerInput.css";
-import { firestore } from "../firebase";
-import { Timestamp, addDoc, collection } from "firebase/firestore";
+import { useState } from 'react';
+import '../css/KilometerInput.css';
+import { firestore } from '../firebase';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+import { THIS_YEAR } from '../utils/constants';
 
 const KilometerInput = () => {
   const [kmCount, setKmCount] = useState(0);
@@ -9,44 +10,43 @@ const KilometerInput = () => {
   const increase = () => setKmCount(kmCount + 1);
   const decrease = () => setKmCount(kmCount - 1);
 
-  const year = new Date().getFullYear(); // Gets current year for naming the collection
-
-  const collectionRef = collection(firestore, `${year} runs`); // Creates collection if one doesnt exist
+  const collectionRef = collection(firestore, `${THIS_YEAR} runs`); // Creates collection if one doesnt exist
 
   const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevents page from refreshing when submitting the form
     try {
       // Quick fix for negative or 0 kilometers being entered.
       if (kmCount <= 0) {
-        alert("Cannot add 0 or negative number");
+        alert('Cannot add 0 or negative number');
       } else {
         addDoc(collectionRef, { Kilometers: kmCount, date: Timestamp.now() }); // Adds kilometers ran and date to collection.
-        console.log("Run successfuly added");
+        console.log('Run successfuly added');
       }
     } catch (e) {
-      console.log("Error adding run: ", e);
+      console.log('Error adding run: ', e);
     }
     setKmCount(0);
   };
 
   return (
-    <div className="kilometerContainer">
-      <p className="inputTitle">Enter your kilometers ran today</p>
-      <form className="counterForm" onSubmit={handleSubmit}>
-        <button className="decreaseButton" type="button" onClick={decrease}>
+    <div className='kilometerContainer'>
+      <p className='inputTitle'>Enter your kilometers ran today</p>
+      <form className='counterForm' onSubmit={handleSubmit}>
+        <button className='decreaseButton' type='button' onClick={decrease}>
           -
         </button>
         <input
-          type="text"
+          type='text'
           value={kmCount}
           readOnly
-          className="counterInput"
-          aria-label="numberInput"
+          className='counterInput'
+          aria-label='numberInput'
         />
-        <button className="increaseButton" type="button" onClick={increase}>
+        <button className='increaseButton' type='button' onClick={increase}>
           +
         </button>
       </form>
-      <button className="submitButton" onClick={handleSubmit}>
+      <button className='submitButton' onClick={handleSubmit}>
         Submit
       </button>
     </div>
