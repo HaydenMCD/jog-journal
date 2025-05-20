1d448c48eb97cb1b72293a2567c66b3be3b046f1
Hayden <haysay424@gmail.com>
2025-05-20 21:15:47 +1200

    Removed Login from app.js. Reduced statsView min width so it is no longer wider than the page.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import "./css/App.css";
 import Header from "./components/Header.js";
 import StatsView from "./components/StatComponents/StatsView.js";
 import KilometerInput from "./components/KilometerInput.js";
-import Login from "./components/Login.jsx";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
+      <Header />
       <div className="wrapper">
         <KilometerInput />
         <StatsView />
-      </div> */}
-      <Login />
+      </div>
     </div>
   );
 }
