0fd99f1d1dbf939aa0c10d6af9f782e5510a950e
Hayden <haysay424@gmail.com>
2025-05-20 21:11:11 +1200

    Created Login files. Added email and password fields and a login button. Aded styling from Signup page.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./css/App.css";
 import Header from "./components/Header.js";
 import StatsView from "./components/StatComponents/StatsView.js";
 import KilometerInput from "./components/KilometerInput.js";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />
       <div className="wrapper">
         <KilometerInput />
         <StatsView />
-      </div>
+      </div> */}
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import "../css/Login.css";
+
+const sendLoginData = () => {
+  window.location.reload();
+};
+
+const Login = () => {
+  return (
+    <form>
+      <div className="login-container">
+        <div className="title">Login</div>
+        <input
+          type="email"
+          className="form-input"
+          name="email-input"
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          className="form-input"
+          name="password-input"
+          placeholder="Password"
+          required
+        />
+        <input
+          type="submit"
+          className="submit-btn"
+          onClick={sendLoginData}
+          value="Login"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Login;
