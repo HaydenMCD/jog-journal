940ee4c941028866253f41c780d7097b8e4b6647
Anthony Legg <anthony.legg@outlook.com>
2025-07-08 15:46:33 +1200

    #8 alter home page so no user data is displayed

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,20 +1,14 @@
 import '../css/App.css';
 import Header from '../components/Header';
-import StatsView from '../components/StatComponents/StatsView';
-import KilometerInput from '../components/KilometerInput';
-import { DistanceProvider } from '../context/useDistanceProvider';
 
 function App() {
   return (
-    <DistanceProvider>
-      <div className='App'>
-        <Header />
-        <div className='wrapper'>
-          <KilometerInput />
-          <StatsView />
-        </div>
-      </div>
-    </DistanceProvider>
+    <main className='App'>
+      <Header />
+      <section className='wrapper'>
+        <h2>Jogger App Public Info Page</h2>
+      </section>
+    </main>
   );
 }
 
