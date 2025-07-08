b3ede734a04aaa35846b5b08e8b91d2db2f278a8
Anthony Legg <anthony.legg@outlook.com>
2025-07-08 15:41:33 +1200

    #8 create auth related pages
    added to a silent `(auth)` sub-directory
    
    (auth) will not be part of the route used

---
+++ src/app/(auth)/forgot-password.jsx
@@ -0,0 +1,9 @@
+function ForgotYourPasswordPage() {
+  return (
+    <section>
+      <h2>Forgot Your Password Page</h2>
+    </section>
+  );
+}
+
+export default ForgotYourPasswordPage;
---
+++ src/app/(auth)/sign-up.jsx
@@ -0,0 +1,9 @@
+function SignUpPage() {
+  return (
+    <section>
+      <h2>Sign Up Page</h2>
+    </section>
+  );
+}
+
+export default SignUpPage;
